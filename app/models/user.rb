5c4cadf5caf1e61b720f5b5ffda2c38f116ad386
Ricardo Alvarez <ri.alvarez.89@icloud.com>
2021-04-21 15:27:22 -0400

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true 
+
+  has_many :stories 
+
+  def is_admin?
+    admin 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :stories
+  get 'my_stories', to: 'stories#my_stories'
+  get 'users/edit/:id', to: 'admin#edit', as: 'edit_user'
+  patch 'users/update/:id', to: 'admin#update', as: 'update_user'
+  
   root 'stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
